48be4266a970bb1c7e4863a8bc89aee0472f9ab8
presylord <kidlat123@gmail.com>
2021-11-23 15:19:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,15 +16,23 @@ var userInfo = [
 var nameLogin = prompt("Enter name:");
 var passLogin = prompt("Enter pass:");
 
-for (var x = 0; x < userInfo.length; x++)
-  if (
-    userInfo[x].username === nameLogin &&
-    userInfo[x].password === passLogin
-  ) {
-    console.log("Login Successful!");
-    break;
-  } else {
-    console.log("Wrong User and Password!");
-  }
+function isUserValid(username, password) {
+  for (var x = 0; x < userInfo.length; x++)
+    if (
+      userInfo[x].username === nameLogin &&
+      userInfo[x].password === passLogin
+    ) {
+      return true;
+    }
+  return false;
+}
 
-//   use forEach for this activity
+function entryGranted(username, password) {
+  if (isUserValid(username, password)) {
+    console.log("Access Granted");
+  } else {
+    console.log("Wrong User/Password");
+  }
+}
+
+entryGranted(nameLogin, passLogin);
